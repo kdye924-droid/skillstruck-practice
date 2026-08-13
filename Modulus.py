9ea7c226f1bcaad873aa6ae7164a029f6db009a5
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:22 -0700

    exported from Skill Struck

---
+++ Modulus.py
@@ -0,0 +1,8 @@
+
+# family = int(input("Number of family members"))
+# oranges = int(input("Number of oranges"))
+# print( oranges % family)
+
+number = int(input("Pick a number that is 10 or greater"))
+last_two_digits =  100
+print(number % last_two_digits)
