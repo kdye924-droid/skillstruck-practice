6d5280795fa3a6179932c08eea94fc92fc30f1b8
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:23 -0700

    exported from Skill Struck

---
+++ string.py
@@ -0,0 +1,14 @@
+# short_quote="Live what you love"
+# long_quote="""One loves because one is loved. no reson is needed for loving."""
+# print( short_quote)
+# print(long_quote)
+
+name1="Keira"
+name2="Tyla"
+name3="Lexi"
+name4="Kate"
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
