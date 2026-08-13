693b22bdedaca0e6fb6638111c65b28613c9c0cb
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:30 -0700

    exported from Skill Struck

---
+++ Python Conditionals.py
@@ -0,0 +1,31 @@
+# userAge=int(input("How old are you?"))
+# license=True
+# if userAge>= 16 and license == True :
+	# print("You are old enough to drive.")
+# else:
+	# print("You are not able to drive.")
+
+# age = 14
+
+# if age > 10:
+    # print("You can go to the movies.")
+# else:
+    # print("You cannot go to the movies.")
+
+
+# rollerCoster=int(input("how tall are you in inches? "))
+
+# height=48
+
+# if rollerCoster>= height:
+    # print( "You can ride the roller coaster."  )
+# else:
+#    print("You can't ride the roller coaster."  )
+
+
+number=int(input("pick a number "))
+
+if number <= 50:
+    print("Fewer than average")
+else:
+    print("More than average")
