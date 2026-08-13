4e092aa2d5bda3142535427d10ff234a8a889cee
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:20 -0700

    exported from Skill Struck

---
+++ Concatenation.py
@@ -0,0 +1,23 @@
+# string1 = "I ride the bus "
+# string2 = "To school every morning "
+# string3 = " the bus ride is quite."
+# together = string1 + string2 + string3
+# print( together)
+
+# string1 = "I like to"
+# string2 = " swim."
+# print(string1 + string2)
+
+# name=input("What is your name? ")
+# age=input("How old are you? ")
+# birthday=input("When is your birthday? ")
+# print( " Hi " + name +  "! You are " + age + " years old and you were born on " + birthday + ".")
+
+# sinckers=str(input("How many sinckers did you get? "))
+# nerds=str(input("How many nerds did you get? "))
+# butterfingers=str(input("How many butterfingers did you get? ")) 
+# print("This year, you got " + sinckers + " sinckers, " + nerds + " nerds, and " + butterfingers + " butterfingers.")
+
+number=input("pick a number")
+word=str(input("pic a word"))
+print(number + word)
