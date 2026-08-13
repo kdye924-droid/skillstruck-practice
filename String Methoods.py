eb0a12a89b9503248f89a0941e0a72f613a12e92
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:25 -0700

    exported from Skill Struck

---
+++ String Methoods.py
@@ -0,0 +1,14 @@
+# string1="I love coding!"
+# print(string1.upper())
+
+# string2="I LIKE THE COLOR BULE."
+# print(string2.lower())
+
+# string3="I want a pet cat."
+# print(string3.replace("cat","dog"))
+
+# shout=input("shout something")
+# print(shout.upper())
+
+message=input("write something with 1")
+print(message.replace("1","one"))
