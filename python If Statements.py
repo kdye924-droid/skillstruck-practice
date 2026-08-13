4712accce2084258fca745d3193d4683a985ebae
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:29 -0700

    exported from Skill Struck

---
+++ python If Statements.py
@@ -0,0 +1,20 @@
+# Dogs=30
+# Cats=25
+# if Dogs> Cats:
+    # print("You have more dogs then cats ")
+# else:
+    # print(" You have more cats then dogs ")
+
+# num1=input(" pick a number ")
+# num2=input(" pick another number ")
+# if num1<num2:
+    # print(num1)
+# else:
+    # print(num2)
+
+positive=int(input("Enter a positive number "))
+if 100 <= positive <= 999:
+    print("Yes")
+else:
+    print("No")
+    
