bae268557d2a25417539557f7577e0a40dbda083
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:30 -0700

    exported from Skill Struck

---
+++ Lists.py
@@ -0,0 +1,10 @@
+# candy=["kitkats","starbrust","M&Ms","nerds"]
+# pieces=[10, 25, 15, 5]
+# print(candy)
+# print(pieces)
+
+# memories=["How many slices of pizza did you eat?", "How many new places did you go?", "Name one friend you hung out with.", "Name one news event that happened this year.", "How many teachers have you had?"]
+# print(memories)
+
+ages=[50,10,6,17]
+print(ages)
