b68cd8a51c51b87f791b337db8efe314d1760fc1
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:26 -0700

    exported from Skill Struck

---
+++ String Methoods2.py
@@ -0,0 +1,17 @@
+# string1="Codeing is fun!"
+# print(string1[4:9])
+
+# string1=" I love coding! "
+# print(string1.split()) space out the string
+
+# string1="How many characters are in this line of code?"# print(len(string1)) count how many characters in string
+
+# string1="Work"
+# print(string1.find("k")) finds the index value
+
+# string=input("say something ")
+# print(string.rfind("g")) # prints index of the second ocurrence of the letter "g"
+
+string=input("say something ")
+print(str(string.find("e")) + "-" + str(string.rfind("e")))
+
