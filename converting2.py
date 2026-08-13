366dcfced6e09f132588b08db039796877f7d0d3
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:19 -0700

    exported from Skill Struck

---
+++ converting2.py
@@ -0,0 +1,13 @@
+name=input('What is your name? ')
+age=input('How old are you? ')
+
+# print(type(age)) #how to check data type 
+
+# print(name+age=='2')  #compares concatenation to a string
+
+# print(name + age) # concatenation
+
+agenum=int(age) #change to interger 
+
+# print(type(agenum)) 
+print(name * agenum)
