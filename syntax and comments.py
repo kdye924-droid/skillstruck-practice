efe117778c7a1f0f46f75dc28664cc031007b373
kdye924-droid <kdye924@evit.edu>
2026-08-13 09:21:16 -0700

    exported from Skill Struck

---
+++ syntax and comments.py
@@ -0,0 +1,9 @@
+#I enjoy the snow
+morning=input("What do you do in the morning? ")
+print(morning)
+
+afternoon=input("What do you do in the aftetrnoon? ")
+print(afternoon)
+
+evening=input(" What do you do in the evening? ")
+print(evening)
